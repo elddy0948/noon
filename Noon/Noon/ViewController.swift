054974fa5084f2020930dc430dbbfd44a479cc40
Joons <elddy0948@gmail.com>
2022-04-29 14:32:38 +0900

    project init

---
+++ Noon/Noon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Noon
+//
+//  Created by 김호준 on 2022/04/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .systemBackground
+  }
+
+
+}
+
